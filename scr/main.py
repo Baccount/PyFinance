84b5917405748dfe99bfd668e9c1a2e714692b23
“Brandon <cassias.long0r@icloud.com>
2022-09-12 20:42:24 -0700

    basic arguments support

--- scr/main.py
+++ scr/main.py
@@ -1,10 +1,12 @@
 from classes.mclass import Finance
 from classes.rclass import ReadFinances
 from classes.sclass import SaveFinances
+from argm.argms import arguments
 
 
 def main():
-    finance = Finance("John", 25, 1000)
+    arg = arguments()
+    finance = Finance(arg.name, arg.age, arg.money)
     SaveFinances().save(finance)
     print(ReadFinances().getFinance())
 
