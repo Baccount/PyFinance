c8ec3773c9e06f033b6ef931ccececa7a8998a34
“Brandon <cassias.long0r@icloud.com>
2022-09-13 12:25:15 -0700

    splash screen added

--- scr/functions/functions.py
+++ scr/functions/functions.py
@@ -1,7 +1,32 @@
 from classes.mclass import Finance
+from pyfiglet import Figlet
 
 def getInput():
     name = input("Enter your name: ")
     age = input("Enter your age: ")
     money = input("Enter your deposit amount: ")
     return Finance(name, age, money)
+
+def showSplash():
+    """
+    Display splash screen
+    """
+
+    clear_screen()
+    title = "Py\n Finance"
+    f = Figlet(font="standard")
+    print(purple(f.renderText(title)))
+
+
+def clear_screen():
+    """
+    Clear the screen
+    """
+
+    print("\033c", end="")
+
+def purple(text):
+    """
+    Make text dark purple
+    """
+    return "\033[95m" + text + "\033[0m"
--- scr/main.py
+++ scr/main.py
@@ -2,12 +2,13 @@ from argm.argms import arguments
 from classes.mclass import Finance
 from classes.rclass import ReadFinances
 from classes.sclass import SaveFinances
-from functions.functions import getInput
+from functions.functions import getInput, showSplash
 
 def main():
     arg = arguments()
     finance = Finance(arg.name, arg.age, arg.money) if arg.name and arg.money else None
     if not finance:
+        showSplash()
         finance = getInput()
     SaveFinances().save(finance)
     print(ReadFinances().getFinance())
