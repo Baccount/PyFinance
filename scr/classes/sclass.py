64dfae92e6c3dd8865d77a89afba150095d801ed
“Brandon <cassias.long0r@icloud.com>
2022-09-19 14:02:06 -0700

    fixed more bugs

--- scr/classes/sclass.py
+++ scr/classes/sclass.py
@@ -20,13 +20,13 @@ class SaveFinances:
         self.c.close()
         self.conn.close()
 
-    def save(self, finance):
+    def save(self, finance, amount):
         # Saving the data into the database.
         self.conn = sqlite3.connect(self.db)
         self.c = self.conn.cursor()
         # check if the name is already in the database
-        self.c.execute("UPDATE finances SET balance = balance + ? WHERE name = ?", (finance.getBalance(), finance.getName().lower()))
-        self.c.execute("INSERT INTO finances (name, balance) SELECT ?, ? WHERE (SELECT Changes() = 0)", (finance.getName(), finance.getBalance()))
+        self.c.execute("UPDATE finances SET balance = balance + ? WHERE name = ?", (finance.getBalance() + amount, finance.getName().lower()))
+        self.c.execute("INSERT INTO finances (name, balance) SELECT ?, ? WHERE (SELECT Changes() = 0)", (finance.getName(), finance.getBalance() + amount))
         self.conn.commit()
         self.c.close()
         self.conn.close()
--- scr/main.py
+++ scr/main.py
@@ -7,17 +7,20 @@ from classes.mclass import Finance
 def main():
     showSplash()
     usr = loginUsr()
+    name = usr.name
     clear_screen()
-    print(purple("Welcome, " + usr.getName() + " you have "))
-    print(purple(str(ReadFinances().getFinance(usr.getName())[0][1]) + " $ in your account"))
+    print(purple("Welcome, " + name + " you have "))
+    print(purple(str(ReadFinances().getFinance(name)[0][1]) + " $ in your account"))
     while True:
         choice = input("1. Deposit money\n2. Withdraw money\n3. Show balance\n4. Exit\n")
         if choice == "1":
-            SaveFinances().save(usr.deposit())
+            clear_screen()
+            amount = int(input("Enter the amount you want to deposit: "))
+            SaveFinances().save(usr, amount)
         # elif choice == "2":
         #     withdraw(usr)
         elif choice == "3":
-            print(ReadFinances().getFinance(usr.getName()))
+            print(ReadFinances().getFinance(name))
             input("Press enter to continue...")
             clear_screen()
         elif choice == "4":
@@ -33,7 +36,6 @@ def main():
 def loginUsr() -> Finance:
     name = input("Enter your name: ")
     usr = Finance(name)
-    SaveFinances().save(usr)
     return usr
 
 if __name__ == "__main__":
